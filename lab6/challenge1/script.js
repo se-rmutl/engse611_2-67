ed7eaaacb4bb7508c356279508f1cca4238ac352
tkeatkaew <thanit@rmutl.ac.th>
2025-03-03 06:24:35 +0700

    lab6_updated

---
+++ lab6/challenge1/script.js
@@ -0,0 +1,7 @@
+<script>
+var parasToChange = document.querySelectorAll("#main p");
+for (var i = 0; i < parasToChange.length; i++) {
+parasToChange[i].style.fontSize = "24px";
+parasToChange[i].style.color = "red";
+}
+</script>
---
+++ lab6/challenge4/script.js
@@ -0,0 +1,7 @@
+<script>
+const navLinks = document.querySelectorAll("nav ul li a");
+for (let i = 0; i < navLinks.length; i++) {
+//navLinks[i].onclick = function(){ alert( this.innerHTML) };
+navLinks[i].addEventListener("click", function () { alert(this.innerHTML) });
+}
+</script>
